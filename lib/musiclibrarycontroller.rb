a1bf27f3524df57ea1af4445d88a3d206047dd7a
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-12 20:38:21 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -74,8 +74,9 @@ def list_songs_by_artist
   
   def play_song
     puts "Which song number would you like to play?" 
-    list_songs
+    songlist= list_songs
     input = gets.strip 
+    songlist.select {|song| song.index == input}
     puts "Playing #{song.name} by #{song.artist.name}"
   end
 
