6d65e6fe2d154146a36e9a3b0717423aaf67b6a7
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-12 20:03:04 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -18,7 +18,10 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    input.gets
+    
+    input.gets.strip
+    
+    
   end
   
   
